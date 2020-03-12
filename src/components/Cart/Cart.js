e08304e02b1679381b01a2b82fbc1bfeda68849d
Hasan71 <mahmud.cse7@gmail.com>
2020-03-13 02:18:55 +0600

    adding final project

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './Cart.css'
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const total = cart.reduce((total, prd) => total+prd.price, 0);
+
+    let shipping = 0;
+    if(total>35){
+        shipping = 0;
+    }
+    else if(total>15){
+        shipping = 4.99;
+    }
+
+    else if(total>0){
+        shipping = 12.99;
+    }
+
+    const tax = (total / 10).toFixed(2);
+
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+
+    function formatNumber(num){
+        const precison = num.toFixed(2);
+        return Number(precison);
+    }
+
+    return (
+        <div className="cart">
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length} </p>
+            <p>Product Price: {formatNumber(total)} </p>
+            <p>Vat+Tax: {tax} </p>
+            <p>Shipping: {shipping} </p>
+            <p>Total Price: {grandTotal} </p>
+
+        </div>
+    );
+};
+
+export default Cart;
